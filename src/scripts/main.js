b9c23ae7dc864887721402f1ecb8148edb83c568
Tony W <bobacrypto2@gmail.com>
2023-02-12 00:45:44 -0800

    Refactor possibleMoves() code into a separate function to reduce code redundancy | Create new Knight class to begin constructing possible moves tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,45 +1,46 @@
 import '../styles/main.css'
+class Knight {
 
-// Calculate possible moves of a position on our chessboard with a given array of 2 positions ex. [3, 3]
-function possibleMoves(pos) {
-    // The spaces between each possible move for a Knight
-    let ones = [-1, 1];
-    let twos = [-2, 2];
-    let moves = [];
+    constructor(start, end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    possibleMoves(pos) {
+        // The spaces between each possible move for a Knight
+        let ones = [-1, 1];
+        let twos = [-2, 2];
+        
+        let oneMoves = this.calculateMoves(pos, ones, twos);
+        let twoMoves = this.calculateMoves(pos, twos, ones);
     
-    // Calculate half of the possible moves involving -1 and 1 for x in [x, y]
-    // This also accounts for the 4 possible variations of -2 and 2 in y of [x, y]
-    ones.forEach(num => {
-        for (let i = 0; i < 2; i++) {
-            let x = num + pos[0];
-            let y = twos[i] + pos[1];
+        return oneMoves.concat(twoMoves);
+    }
 
-            if(x > 7 || y > 7) {
-                return;
-            } else {
-                let newMove = [x, y];
-                moves.push(newMove);
+    calculateMoves(pos, firstArr, secondArr) {
+        let moves = [];
+        // Calculate half of the possible moves involving -1 and 1 for x in [x, y]
+        // This also accounts for the 4 possible variations of -2 and 2 in y of [x, y]
+        firstArr.forEach(num => {
+            for (let i = 0; i < 2; i++) {
+                let x = num + pos[0];
+                let y = secondArr[i] + pos[1];
+    
+                // Prevents Knight from moving outside of our 8x8 board
+                if(x > 7 || y > 7) {
+                    return;
+                } else {
+                    let newMove = [x, y];
+                    moves.push(newMove);
+                }
             }
-        }
-    });
+        });
 
-    twos.forEach(num => {
-        for (let i = 0; i < 2; i++) {
-            let x = num + pos[0];
-            let y = ones[i] + pos[1];
-
-            if(x > 7 || y > 7) {
-                return;
-            } else {
-                let newMove = [x, y];
-                moves.push(newMove);
-            }
-        }
-    });
-
-    return moves;
+        return moves;
+    }
 }
+// Calculate possible moves of a position on our chessboard with a given array of 2 positions ex. [3, 3]
 
-let test = possibleMoves([6, 3]);
+let test = new Knight();
 
-console.log(test)
+console.log(test.possibleMoves([6, 3]));
