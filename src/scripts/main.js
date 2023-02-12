42c736f55d60d519706af259244b40136f5d3fbf
Tony W <bobacrypto2@gmail.com>
2023-02-12 01:48:30 -0800

    Fix bug in calculateMoves() function where the loop was breaking every time x or y positions dipped below 0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,18 @@
 import '../styles/main.css'
 class Knight {
 
-    constructor(start, end) {
+    constructor(start, end, position, move0, move1, move2, move3, move4, move5, move6, move7) {
         this.start = start;
         this.end = end;
+        this.position = position;
+        this.move0 = move0;
+        this.move1 = move1;
+        this.move2 = move2;
+        this.move3 = move3;
+        this.move4 = move4;
+        this.move5 = move5;
+        this.move6 = move6;
+        this.move7 = move7;
     }
 
     possibleMoves(pos) {
@@ -25,10 +34,13 @@ class Knight {
             for (let i = 0; i < 2; i++) {
                 let x = num + pos[0];
                 let y = secondArr[i] + pos[1];
+
+                console.log(x)
+                console.log(y)
     
                 // Prevents Knight from moving outside of our 8x8 board
-                if(x > 7 || y > 7) {
-                    return;
+                if(x > 7 || y > 7 || x < 0 || y < 0) {
+                    continue;
                 } else {
                     let newMove = [x, y];
                     moves.push(newMove);
@@ -38,9 +50,45 @@ class Knight {
 
         return moves;
     }
+
+    appendMoves(pos, node = this, counter = 0) {
+        console.log(test);
+        if(counter >= 3 || pos == undefined) {
+            return;
+        }
+
+        let moves = this.possibleMoves(pos);
+
+        for (let i = 0; i < moves.length; i++) {
+            node[`move${i}`] = new createMoves(moves[i]);
+        }
+
+        counter++
+        console.log(pos)
+        console.log(node)
+
+        return this.appendMoves(pos = node[`move${counter + 1}`].position, node = node[`move${counter + 1}`], counter);
+
+    }
+}
+
+class createMoves {
+    constructor(position, move0, move1, move2, move3, move4, move5, move6, move7) {
+        this.position = position;
+        this.move0 = move0;
+        this.move1 = move1;
+        this.move2 = move2;
+        this.move3 = move3;
+        this.move4 = move4;
+        this.move5 = move5;
+        this.move6 = move6;
+        this.move7 = move7;
+    }
 }
 // Calculate possible moves of a position on our chessboard with a given array of 2 positions ex. [3, 3]
 
-let test = new Knight();
+let test = new Knight([0, 0], [3, 3], [0, 0]);
+let test2 = new createMoves();
 
-console.log(test.possibleMoves([6, 3]));
+test.appendMoves([7, 1]);
+console.log(test);
